cf24719a13725b33d52e2613a1284264d48c52f9
Felipe Neiva <felipec798@gmail.com>
2019-12-01 14:57:15 -0300

    Corrigindo expressao cron

--- PFC.SGP.UI/MailStartup.cs
+++ PFC.SGP.UI/MailStartup.cs
@@ -20,10 +20,8 @@ namespace PFC.SGP.UI
             GlobalConfiguration.Configuration.UseUnityActivator(container);
 
             //"0 30 7 1/10 * ?"
-            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), Cron.Minutely);
-            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), "0 35 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
-            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), "0 30 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
-            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), "0 35 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
+            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), "0 30 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
+            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), "0 35 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
 
             app.UseHangfireDashboard();
             app.UseHangfireServer();
