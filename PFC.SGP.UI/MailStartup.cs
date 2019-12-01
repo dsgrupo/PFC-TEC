0747cd43fcd9ae677402589362ad3e6d3151325e
Felipe Neiva <felipec798@gmail.com>
2019-12-01 14:51:08 -0300

    Corrigindo a expressao Cron

--- PFC.SGP.UI/MailStartup.cs
+++ PFC.SGP.UI/MailStartup.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin;
 using Owin;
 using PFC.SGP.Service;
+using System;
 
 [assembly: OwinStartup(typeof(PFC.SGP.UI.MailStartup))]
 
@@ -9,16 +10,21 @@ namespace PFC.SGP.UI
 {
     public class MailStartup
     {
+
         public void Configuration(IAppBuilder app)
         {
+
             var container = UnityConfig.GetConfiguredContainer();
 
             GlobalConfiguration.Configuration.UseSqlServerStorage("devConn");
             GlobalConfiguration.Configuration.UseUnityActivator(container);
 
-            // Para um tempo maior, podemos utilizar Cron.DayInterval(10)
-            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), Cron.DayInterval(10));
-            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), Cron.DayInterval(10));
+            //"0 30 7 1/10 * ?"
+            RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), Cron.Minutely);
+            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), "0 35 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
+            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarNotificacaoPorEmail(), "0 30 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
+            //RecurringJob.AddOrUpdate<MailService>(x => x.EnviarEmailParaAlunos(), "0 35 13 1/10 * ?", TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"));
+
             app.UseHangfireDashboard();
             app.UseHangfireServer();
 
